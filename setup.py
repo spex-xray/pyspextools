caca9d1d96763475e17ad0c859f7205cf44b5692
Jelle de Plaa <j.de.plaa@sron.nl>
2019-06-11 13:25:01 +0200

    Changed the way sphinx integrates with Python setuptools. The previous method was depricated by the Sphinx developers. Now the documentation can be generated with the command: ```python setup.py build_sphinx```.

--- setup.py
+++ setup.py
@@ -14,8 +14,19 @@ for d, folders, files in os.walk('doc/build/html'):
     dir_inst='doc/'+re.sub('doc/build/','',d)
     datafiles.append((dir_inst, [os.path.join(d,f) for f in files]))
 
+# Set up sphinx
+try:
+    from sphinx.setup_command import BuildDoc
+    cmdclass = {'build_sphinx': BuildDoc}
+
+except ImportError:
+    print("Unable to generate documentation. Install sphinx")
+    sys.exit(0)
+
+name = 'pyspextools'
+
 # Do the actual setup
-setup(name='pyspextools',
+setup(name=name,
       version=pyspextools.__version__,
       description='SPEX Python tools',
       author='SPEX development team',
@@ -27,19 +38,11 @@ setup(name='pyspextools',
       scripts=['scripts/ogipgenrsp','scripts/ogip2spex','scripts/simres','scripts/tg2spex'],
       data_files=datafiles,
       install_requires=dependencies,
+      cmdclass=cmdclass,
+      command_options={
+            'build_sphinx': {
+            'project': ('setup.py', name),
+            'version': ('setup.py', pyspextools.__version__),
+            'release': ('setup.py', ''),
+            'source_dir': ('setup.py', './doc/source')}},
       )
-
-# Auto-generate documentation
-try:
-    import sphinx
-
-    # Automatically build html documentation
-    # For example:
-    #   format = 'html'
-    #   sphinx-src-dir = './doc'
-    #   sphinx-build-dir = './doc/build'
-    sphinx.build_main(['setup.py', '-b', 'html', './doc/source', './doc/build/html'])
-
-except ImportError:
-    print("Unable to generate documentation. Install sphinx")
-
