c78df426f336a253ee69b485e97824ddbb29b4e8
Jelle de Plaa <j.de.plaa@sron.nl>
2019-06-12 14:53:35 +0200

    Add version option to all scripts. Add copyright and Github page to the header message of scripts. Add Github documentation link to help option. Add Github page and license to setup.py. Remove automatic building of documentation in setup.py (now done by command ```python setup.py build_sphinx```).

--- pyspextools/messages.py
+++ pyspextools/messages.py
@@ -14,6 +14,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import sys
+import pyspextools
 from pyspextools.color import Colors
 
 # Stuff to import for compatibility between python 2 and 3
@@ -22,9 +23,16 @@ from future import standard_library
 
 standard_library.install_aliases()
 
+# Set general messages for argument parsing
+docs = 'See full documentation at: https://spex-xray.github.io/pyspextools'
+version = '%(prog)s {:s} (C) 2018-2019, Jelle de Plaa, SRON Netherlands Institute for Space Research, ' \
+          'Apache 2.0 License'.format(pyspextools.__version__)
+
+# Initialize colors
 color = Colors()
 
 def set_color(set):
+    '''Set whether text output can be colored or not.'''
     if set:
         color.set_color(True)
     else:
@@ -32,10 +40,12 @@ def set_color(set):
 
 # Create methods to do show processes and their result
 def proc_start(text):
+    '''Print text to terminal at the start of the procedure.'''
     print(text+'... ', end='')
     sys.stdout.flush()
 
 def proc_end(result):
+    '''Report the result of a procedure to terminal.'''
     if result == 0:
         print(color.OKGREEN+"OK"+color.ENDC)
     else:
@@ -47,4 +57,14 @@ def warning(text):
 
 def error(text):
     print(color.FAIL+"ERROR "+color.ENDC+text)
+
+def print_header(scriptname):
+    '''Print the header when executing a script.'''
+    print("==================================")
+    print(" This is {:s} version {:s}".format(scriptname, pyspextools.__version__))
+    print("==================================")
+    print("(C) 2018-2019 Jelle de Plaa")
+    print("SRON Netherlands Institute for Space Research")
+    print("Github: https://github.com/spex-xray/pyspextools")
+    print("")
 
--- setup.py
+++ setup.py
@@ -8,20 +8,14 @@ import pyspextools
 # Set dependencies, we need sphinx to build doc and numpy for arrays
 dependencies = ['sphinx','numpy','future','astropy','sphinx-argparse']
 
-# Add doc directory to install-prefix directory
-datafiles=[]
-for d, folders, files in os.walk('doc/build/html'):
-    dir_inst='doc/'+re.sub('doc/build/','',d)
-    datafiles.append((dir_inst, [os.path.join(d,f) for f in files]))
-
 # Set up sphinx
 try:
     from sphinx.setup_command import BuildDoc
     cmdclass = {'build_sphinx': BuildDoc}
 
 except ImportError:
-    print("Unable to generate documentation. Install sphinx")
-    sys.exit(0)
+    print("Cannot generate documentation. Please install sphinx.")
+    cmdclass = {}
 
 name = 'pyspextools'
 
@@ -31,12 +25,11 @@ setup(name=name,
       description='SPEX Python tools',
       author='SPEX development team',
       author_email='j.de.plaa@sron.nl',
-      url='https://www.sron.nl/spex',
+      url='https://github.com/spex-xray/pyspextools',
+      license='Apache 2.0',
       packages=find_packages(),
       include_package_data = True,
-      package_data={'':['*.rst']},
       scripts=['scripts/ogipgenrsp','scripts/ogip2spex','scripts/simres','scripts/tg2spex'],
-      data_files=datafiles,
       install_requires=dependencies,
       cmdclass=cmdclass,
       command_options={
