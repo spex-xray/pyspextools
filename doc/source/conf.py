eb22cedf1dbb9c378f621f9acfb395c6ff5f0389
Jelle de Plaa <j.de.plaa@sron.nl>
2023-11-15 15:20:00 +0100

    Update copyright years

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pyspextools'
-copyright = '2018-2023, SRON Netherlands Institute for Space Research'
+copyright = '2018-2024, SRON Netherlands Institute for Space Research'
 author = 'Jelle de Plaa'
 
 # The version info for the project you're documenting, acts as replacement for
