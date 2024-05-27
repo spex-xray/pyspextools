5fd9de6d269c0314ef2b1ce0abcf5c3a0e1583fd
Jelle de Plaa <j.de.plaa@sron.nl>
2024-05-27 13:28:22 +0200

    Update script headers

--- pyspextools/messages.py
+++ pyspextools/messages.py
@@ -87,7 +87,7 @@ def print_header(scriptname):
     print("==================================")
     print(" This is {:s} version {:s}".format(scriptname, pyspextools.__version__))
     print("==================================")
-    print("(C) 2018-2023 Jelle de Plaa")
+    print("(C) 2018-2024 Jelle de Plaa")
     print("SRON Netherlands Institute for Space Research")
     print("Github: https://github.com/spex-xray/pyspextools")
     print("")
